b9ff3c56b3b7836e6323779851ff5ef0e5ab41e2
Matthew McCormick <mccormick.ms@gmail.com>
2018-04-30 23:24:18 -0700

    add shifter module
    added shifter module to dp_parts. Still need to add connections in datapath file and test

--- sources/dp_parts.v
+++ sources/dp_parts.v
@@ -92,3 +92,92 @@ module regfile
     assign rd2 = (ra2 == 0) ? 0 : rf[ra2];
     assign rd3 = (ra3 == 0) ? 0 : rf[ra3];
 endmodule
+
+module shifter
+(input  shift_en, shift_dir, 
+        [4:0]  shamt,
+        [31:0] rd1_pre,
+ output [31:0] rd1_pst);
+
+always @(*) 
+begin
+    if (shift_en && shift_dir) // if shift_dir = 1, shift left 
+        begin
+            case(shamt)
+                5'b00000 : rd1_pst = rd1_pre;
+                5'b00001 : rd1_pst = {rd1_pre, {1'b0}};
+                5'b00010 : rd1_pst = {rd1_pre, {2'b0}};
+                5'b00011 : rd1_pst = {rd1_pre, {3'b0}};
+                5'b00100 : rd1_pst = {rd1_pre, {4'b0}};
+                5'b00101 : rd1_pst = {rd1_pre, {5'b0}};
+                5'b00110 : rd1_pst = {rd1_pre, {6'b0}};
+                5'b00111 : rd1_pst = {rd1_pre, {7'b0}};
+                5'b01000 : rd1_pst = {rd1_pre, {8'b0}};
+                5'b01001 : rd1_pst = {rd1_pre, {9'b0}};
+                5'b01010 : rd1_pst = {rd1_pre, {10'b0}};
+                5'b01011 : rd1_pst = {rd1_pre, {11'b0}};
+                5'b01100 : rd1_pst = {rd1_pre, {12'b0}};
+                5'b01101 : rd1_pst = {rd1_pre, {13'b0}};
+                5'b01110 : rd1_pst = {rd1_pre, {14'b0}};
+                5'b01111 : rd1_pst = {rd1_pre, {15'b0}};
+                5'b10000 : rd1_pst = {rd1_pre, {16'b0}};
+                5'b10001 : rd1_pst = {rd1_pre, {17'b0}};
+                5'b10010 : rd1_pst = {rd1_pre, {18'b0}};
+                5'b10011 : rd1_pst = {rd1_pre, {19'b0}};
+                5'b10100 : rd1_pst = {rd1_pre, {20'b0}};
+                5'b10101 : rd1_pst = {rd1_pre, {21'b0}};
+                5'b10110 : rd1_pst = {rd1_pre, {22'b0}};
+                5'b10111 : rd1_pst = {rd1_pre, {23'b0}};
+                5'b11000 : rd1_pst = {rd1_pre, {24'b0}};
+                5'b11001 : rd1_pst = {rd1_pre, {25'b0}};
+                5'b11010 : rd1_pst = {rd1_pre, {26'b0}};
+                5'b11011 : rd1_pst = {rd1_pre, {27'b0}};
+                5'b11100 : rd1_pst = {rd1_pre, {28'b0}};
+                5'b11101 : rd1_pst = {rd1_pre, {29'b0}};
+                5'b11110 : rd1_pst = {rd1_pre, {30'b0}};
+                5'b11111 : rd1_pst = {rd1_pre, {31'b0}};
+            endcase
+        end
+    else if (shift_en && !shift_dir) // if shift_dir = 0, shift right
+        begin
+            case(shamt)
+                5'b00000 : rd1_pst = rd1_pre;
+                5'b00001 : rd1_pst = {{1'b0}, rd1_pre[31:1]};
+                5'b00010 : rd1_pst = {{2'b0}, rd1_pre[ 31:2]};
+                5'b00011 : rd1_pst = {{3'b0}, rd1_pre[ 31:3]};
+                5'b00100 : rd1_pst = {{4'b0}, rd1_pre[ 31:4]};
+                5'b00101 : rd1_pst = {{5'b0}, rd1_pre[ 31:5]};
+                5'b00110 : rd1_pst = {{6'b0}, rd1_pre[ 31:6]};
+                5'b00111 : rd1_pst = {{7'b0}, rd1_pre[ 31:7]};
+                5'b01000 : rd1_pst = {{8'b0}, rd1_pre[ 31:8]};
+                5'b01001 : rd1_pst = {{9'b0}, rd1_pre[ 31:9]};
+                5'b01010 : rd1_pst = {{10'b0}, rd1_pre[ 31:10]};
+                5'b01011 : rd1_pst = {{11'b0}, rd1_pre[ 31:11]};
+                5'b01100 : rd1_pst = {{12'b0}, rd1_pre[ 31:12]};
+                5'b01101 : rd1_pst = {{13'b0}, rd1_pre[ 31:13]};
+                5'b01110 : rd1_pst = {{14'b0}, rd1_pre[ 31:14]};
+                5'b01111 : rd1_pst = {{15'b0}, rd1_pre[ 31:15]};
+                5'b10000 : rd1_pst = {{16'b0}, rd1_pre[ 31:16]};
+                5'b10001 : rd1_pst = {{17'b0}, rd1_pre[ 31:17]};
+                5'b10010 : rd1_pst = {{18'b0}, rd1_pre[ 31:18]};
+                5'b10011 : rd1_pst = {{19'b0}, rd1_pre[ 31:19]};
+                5'b10100 : rd1_pst = {{20'b0}, rd1_pre[ 31:20]};
+                5'b10101 : rd1_pst = {{21'b0}, rd1_pre[ 31:21]};
+                5'b10110 : rd1_pst = {{22'b0}, rd1_pre[ 31:22]};
+                5'b10111 : rd1_pst = {{23'b0}, rd1_pre[ 31:23]};
+                5'b11000 : rd1_pst = {{24'b0}, rd1_pre[ 31:24]};
+                5'b11001 : rd1_pst = {{25'b0}, rd1_pre[ 31:25]};
+                5'b11010 : rd1_pst = {{26'b0}, rd1_pre[ 31:26]};
+                5'b11011 : rd1_pst = {{27'b0}, rd1_pre[ 31:27]};
+                5'b11100 : rd1_pst = {{28'b0}, rd1_pre[ 31:28]};
+                5'b11101 : rd1_pst = {{29'b0}, rd1_pre[ 31:29]};
+                5'b11110 : rd1_pst = {{30'b0}, rd1_pre[ 31:30]};
+                5'b11111 : rd1_pst = {{31'b0}, rd1_pre[31]};
+            endcase
+        end
+    else 
+        begin
+            rd1_pst = rd1_pre;   
+        end
+end
+endmodule
