af6aebdeba27db64c7f7b932aa2eff1b7e811f9c
Alex Norell <alex@alexnorell.com>
2018-05-03 11:39:57 -0700

    Removed empty header descriptions

--- sources/Factorial.v
+++ sources/Factorial.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 02/03/2018 11:21:42 PM
-// Design Name: 
-// Module Name: Factorial
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Factorial(
     input GO, clk, rst,
