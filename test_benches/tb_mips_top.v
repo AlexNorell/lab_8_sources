f42eed348ff92a16264e5b7c9b38b9031de94f26
Matthew McCormick <mccormick.ms@gmail.com>
2018-05-02 23:12:09 -0700

    add interface wrapper signals to mips_top DUT

--- test_benches/tb_mips_top.v
+++ test_benches/tb_mips_top.v
@@ -3,7 +3,7 @@ module tb_mips_top;
     wire        we_dm;
     wire [31:0] pc_current, instr, alu_out, wd_dm, rd_dm;
     
-    mips_top DUT (clk, rst, we_dm, pc_current, instr, alu_out, wd_dm, rd_dm);
+    mips_top DUT (clk, rst, gpi1, gpi2, we_dm, pc_current, instr, alu_out, wd_dm, rd_dm, gpo1, gpo2);
     
     task tick; begin #5 clk = 1; #5 clk = 0; end endtask
     task rest; begin #5 rst = 1; #5 rst = 0; end endtask
